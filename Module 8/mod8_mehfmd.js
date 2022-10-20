09632587522fa4c3b125aac2bf4ee79819650d49
Madison Hassler <mehfmd@umsystem.edu>
2022-10-20 10:35:42 -0500

    Mod7 and solidworks

--- Module 8/mod8_mehfmd.js
+++ Module 8/mod8_mehfmd.js
@@ -13,9 +13,9 @@
 // Note: You are calculating the number of orders and not quantity ordered. 
 // For example, if Alice orders 3 pizzas today and 5 pizzas next week then pizza orders equals 2 and the quantity of pizzas ordered equals 8.
 db.orders.aggregate([
-    {$unwind : "$orderDetails"},
-    {$group: {_id: "$orderDetails.productName", "Number of Orders": {$sum:1}}},
-    {$sort: {"Number of Orders": -1}}
+    { $unwind: "$orderDetails" },
+    { $group: { _id: "$orderDetails.productName", "Number of Orders": { $sum: 1 } } },
+    { $sort: { "Number of Orders": -1 } }
 ])
 
 
@@ -23,36 +23,36 @@ db.orders.aggregate([
 // Display the product name and quantity ordered in a column called “Quantity Ordered”. 
 // Display the results in descending order based on Quantity Ordered.
 db.orders.aggregate([
-    {$unwind : "$orderDetails"},
-    {$group: {_id: "$orderDetails.productName", "Quantity Ordered":{$sum:"$orderDetails.quantityOrdered"}}},
-    {$sort: {"Quantity Ordered": -1}}
+    { $unwind: "$orderDetails" },
+    { $group: { _id: "$orderDetails.productName", "Quantity Ordered": { $sum: "$orderDetails.quantityOrdered" } } },
+    { $sort: { "Quantity Ordered": -1 } }
 ])
 
 
 // 3. Calculate the total dollar value of the top 25 products that has been ordered in the database. 
 // Display the product name and the dollar value in a column called “Total Value”.
 db.orders.aggregate([
-    {$unwind : "$orderDetails"},
-    {$group: {_id: "$orderDetails.productName", "Total Value":{$sum: {$multiply:["$orderDetails.quantityOrdered", "$orderDetails.priceEach"]}}}},
-    {$sort:{"Total Value": -1}},
-    {$limit: 25},
+    { $unwind: "$orderDetails" },
+    { $group: { _id: "$orderDetails.productName", "Total Value": { $sum: { $multiply: ["$orderDetails.quantityOrdered", "$orderDetails.priceEach"] } } } },
+    { $sort: { "Total Value": -1 } },
+    { $limit: 25 },
 ])
 
 
 // 4. Calculate the number of orders each customer has placed and display the top 25 in descending order based on orders placed. 
 // Display the customer name and the orders placed in a columns called “Orders Placed”.
 db.orders.aggregate([
-    {$group: {_id: "$customerName", "Orders Placed":{$sum: 1}}},
-    {$sort: {"Orders Placed": -1}},
-    {$limit: 25}
+    { $group: { _id: "$customerName", "Orders Placed": { $sum: 1 } } },
+    { $sort: { "Orders Placed": -1 } },
+    { $limit: 25 }
 ])
 
 
 // 5. Calculate the total payments made each year. 
 // Display the year and total payments in a column called “Total Payments”.
 db.customers.aggregate([
-    {$unwind: "$payments"},
-    {$group: {"Total Payments": {$sum: "$payments.amount"}, _id: {$year: "$payments.paymentDate"}}}
+    { $unwind: "$payments" },
+    { $group: { _id: { $year: "$payments.paymentDate" }, "Total Payments": { $sum: "$payments.amount" } } }
 ])
 
 
@@ -61,11 +61,11 @@ db.customers.aggregate([
 // Order the results by month in ascending order. 
 // **Hint: You will need to use the $addFields to add a field for year using the $year function, then match for 2004.
 db.customers.aggregate([
-    {$unwind: "$payments"},
-    {$addFields:{"year":{$year: "$payments.paymentDate"}}},
-    {$match: {year: 2004}},
-    {$group: {totalPayments: {$sum: "$payments.amount"}, _id: {$month: "$payments.paymentDate"}}},
-    {$sort: {_id: 1}}
+    { $unwind: "$payments" },
+    { $addFields: { "year": { $year: "$payments.paymentDate" } } },
+    { $match: { year: 2004 } },
+    { $group: { _id: { $month: "$payments.paymentDate" }, "Total Payments": { $sum: "$payments.amount" } } },
+    { $sort: { _id: 1 } }
 ])
 
 
@@ -74,11 +74,11 @@ db.customers.aggregate([
 // Order the results by day in ascending order. 
 // **Hint: You will need to use the $addFields to add a field for year and month, using the $year and $month functions, then match for 2004 and 12.
 db.customers.aggregate([
-    {$unwind:"$payments"},
-    {$addFields:{"year":{$year: "$payments.paymentDate"},"month":{$month: "$payments.paymentDate"}}},
-    {$match:{"year": 2004,"month":12}},
-    {$group: {_id:{$dayOfMonth: "$payments.paymentDate"}, totalPayments: {$sum: "$payments.amount"}}},
-    {$sort: {_id: 1}}
+    { $unwind: "$payments" },
+    { $addFields: { "year": { $year: "$payments.paymentDate" }, "month": { $month: "$payments.paymentDate" } } },
+    { $match: { "year": 2004, "month": 12 } },
+    { $group: { _id: { $dayOfMonth: "$payments.paymentDate" }, "Total Payments": { $sum: "$payments.amount" } } },
+    { $sort: { _id: 1 } }
 ])
 
 
@@ -86,9 +86,9 @@ db.customers.aggregate([
 // Display the customer name and the total payments in a column named “Total Payments”. 
 // Order the results by total payments.
 db.customers.aggregate([
-    {$unwind: "$payments"},
-    {$group: {"Total Payments": {$sum: "$payments.amount"}, _id: "$customerName"}},
-    {$sort: {"Total Payments": -1}}
+    { $unwind: "$payments" },
+    { $group: { _id: "$customerName", "Total Payments": { $sum: "$payments.amount" } } },
+    { $sort: { "Total Payments": -1 } }
 ])
 
 
@@ -96,22 +96,22 @@ db.customers.aggregate([
 // Display the customer name and and number of customers in each state in a column called “Number of Customers in State”. 
 // Sort the results by the Number of Customers in State.
 db.customers.aggregate([
-    {$group: {_id: "$state", "Number of Customers in State":{$sum: 1}}},
-    {$sort: {"Number of Customers in State": -1}}
+    { $group: { _id: "$state", "Number of Customers in State": { $sum: 1 } } },
+    { $sort: { "Number of Customers in State": -1 } }
 ])
 
 
 // 10. Which employees manage the most people? Develop a query to calculate the number of people each employees manages. 
 // Display the employee name and number of employees employees they manage in a column called “Number of Reports”.
 db.employees.aggregate([
-    {$group: {_id: "$reportsTo", "Number of Reports":{$sum: 1}}}
+    { $group: { _id: "$reportsTo", "Number of Reports": { $sum: 1 } } }
 ])
 
 
 // 11. Write a query to calculate the number of product lines in the database. 
 // Display the result in a column called “Number of Lines”.
 db.products.aggregate([
-    {$group:{_id: "$productLine.line", "Number of Lines": {$sum: 1}}},
+    { $group: { _id: "$productLine.line", "Number of Lines": { $sum: 1 } } },
 ])
 
 
@@ -120,6 +120,6 @@ db.products.aggregate([
 // Display the product name, quantity in stock, buy price, and in its dollar value in a column called “Dollar Value”. 
 // Sort the results in descending order based on dollar value.
 db.products.aggregate([
-    {$group: {_id: "$productName", dollarValue:{$sum: {$multiply:["$quantityInStock", "$buyPrice"]}}}},
-    {$sort:{"dollarValue": -1}}
+    { $group: { _id: "$productName", "Dollar Value": { $sum: { $multiply: ["$quantityInStock", "$buyPrice"] } } } },
+    { $sort: { "Dollar Value": -1 } }
 ])
